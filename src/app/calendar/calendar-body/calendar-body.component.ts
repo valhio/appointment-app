aadedc55c0a1a6d6df7b259b0459929516808fd3
valhio <valentingerasimow@gmail.com>
2023-02-09 21:36:36 +0200

    Closes #6. Button now shows as selected when selected

--- src/app/calendar/calendar-body/calendar-body.component.ts
+++ src/app/calendar/calendar-body/calendar-body.component.ts
@@ -8,19 +8,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class CalendarBodyComponent {
 
   @Input() calendarMatrix: any[] | undefined;
-  @Input() currentDate: Date | null | undefined;
+  @Input() selectedDate: Date | null | undefined;
 
   @Output() changeSelectedDate = new EventEmitter<Date>();
   @Output() showBookingHoursEvent = new EventEmitter<boolean>();
 
   public days = ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
 
-  isSameDate(date1: Date, date2: Date): boolean {
-    return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
+  isSelectedDate(date1: Date): boolean {
+    return date1.getFullYear() === this.selectedDate?.getFullYear()
+      && date1.getMonth() === this.selectedDate?.getMonth()
+      && date1.getDate() === this.selectedDate?.getDate();
   }
 
   isToday(date: Date): boolean {
-    return this.isSameDate(date, new Date());
+    const today = new Date();
+    return date.getFullYear() === today.getFullYear()
+      && date.getMonth() === today.getMonth()
+      && date.getDate() === today.getDate();
   }
 
   onChangeSelectedDate(date: Date): void {
