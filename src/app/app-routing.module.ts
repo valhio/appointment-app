0cff0dbd6689cc9fd04c9f7206ee5aa872bd7b3b
valhio <valentingerasimow@gmail.com>
2023-02-16 17:47:19 +0200

    Closes #10. Removed appointment feature from home page and moved into a separate route/component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarManagementComponent } from './components/management/calendar-management/calendar-management.component';
+import { AppointmentComponent } from './components/home/appointment/appointment.component';
 
 const routes: Routes = [
   {path: '', component: CalendarComponent},
   {path: 'home', component: CalendarComponent},
   {path: 'management', component: CalendarManagementComponent},
+  {path: 'appointment', component: AppointmentComponent},
   {path: '**', redirectTo: '/home', pathMatch: 'full'},
 
 ];
