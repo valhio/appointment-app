2399be0401159f55d0efdec8682cecc3ecb05fe4
valhio <valentingerasimow@gmail.com>
2023-02-21 14:48:49 +0200

    UI changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  openGoogleMaps() {
+    window.open('https://maps.app.goo.gl/bu8iN1tRXcVpa6eu5', '_blank');
+  }
 }
