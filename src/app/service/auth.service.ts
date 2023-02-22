4488fa1e0eab22e7bc2772fa2c78ec9576b09413
valhio <valentingerasimow@gmail.com>
2023-02-22 21:47:13 +0200

    Auth service  done

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  userLoggedIn: boolean = false;
+
+  constructor(private router: Router, private afAuth: AngularFireAuth) {
+    this.userLoggedIn = false;
+    this.afAuth.onAuthStateChanged((user) => {
+      if(user){
+        this.userLoggedIn = true;
+      }else{
+        this.userLoggedIn = false;
+      }
+    }
+    )
+  }
+
+  loginUser(email: string, password: string){
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+    .then((res) => {
+      this.userLoggedIn = true;
+      return null;
+    }
+    ).catch((error) => {
+      this.userLoggedIn = false;
+      return error;
+    }
+    )
+  }
+}
