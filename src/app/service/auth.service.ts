d1abad4d73e0272e9da0e9e1cb05f4bdde3a033e
valhio <valentingerasimow@gmail.com>
2023-02-22 22:10:59 +0200

    Closes #24. Management authentication done.
    Management component now requires authentication. Only an authenticated
    user can acces the page. Also configured firestore rules to restrict updates
    and deletes access. Now, they are only available for authenticated users.

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,25 +12,25 @@ export class AuthService {
   constructor(private router: Router, private afAuth: AngularFireAuth) {
     this.userLoggedIn = false;
     this.afAuth.onAuthStateChanged((user) => {
-      if(user){
+      if (user) {
         this.userLoggedIn = true;
-      }else{
+      } else {
         this.userLoggedIn = false;
       }
     }
     )
   }
 
-  loginUser(email: string, password: string){
+  loginUser(email: string, password: string) {
     return this.afAuth.signInWithEmailAndPassword(email, password)
-    .then((res) => {
-      this.userLoggedIn = true;
-      return null;
-    }
-    ).catch((error) => {
-      this.userLoggedIn = false;
-      return error;
-    }
-    )
+      .then((res) => {
+        this.userLoggedIn = true;
+        return null;
+      }
+      ).catch((error) => {
+        this.userLoggedIn = false;
+        return error;
+      }
+      )
   }
 }
